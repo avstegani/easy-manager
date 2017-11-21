70c27ac1950a486743ce8229a7e113ee9c4890db
avstegani <33634840+avstegani@users.noreply.github.com>
2017-11-21 11:15:45 -0200

    Update sampleTest.js

--- public/tests/functional/scripts/sampleTest.js
+++ public/tests/functional/scripts/sampleTest.js
@@ -2,10 +2,9 @@ module.exports = {
   'Sample functional test' : function (browser) {
     browser
       .url('https://easy-manager.mybluemix.net')
-      .waitForElementVisible('input.exampleInputEmail1', 60000, true,
+      .waitForElementPresent('#exampleInputEmail1', 60000, true,
         function(){}, 'Waiting for e-mail Input to become visible')
-      .setValue('input.exampleInputEmail1', 'test@br.ibm.com')
-      .waitForElementVisible('.modal-body', 60000)
+      .setValue('#exampleInputEmail1', 'test@br.ibm.com')
       .pause(5000)
       .end();
   }
